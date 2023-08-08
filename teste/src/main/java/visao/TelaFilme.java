a40a203695ceec82f50e945729193f7db4ceff9a
Ash <ash.m10@aluno.ifsc.edu.br>
2023-08-08 09:39:25 -0300

    tela

--- teste/src/main/java/visao/TelaFilme.java
+++ teste/src/main/java/visao/TelaFilme.java
@@ -7,11 +7,14 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class TelaFilme extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtFilme;
+	private JTextField txtDiretor;
+	private JTextField txtGenero;
 
 	/**
 	 * Launch the application.
@@ -34,7 +37,7 @@ public class TelaFilme extends JFrame {
 	 */
 	public TelaFilme() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 373, 276);
+		setBounds(100, 100, 310, 276);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -53,5 +56,23 @@ public class TelaFilme extends JFrame {
 		JLabel lblDiretor = new JLabel("Diretor: ");
 		lblDiretor.setBounds(63, 82, 46, 14);
 		contentPane.add(lblDiretor);
+		
+		txtDiretor = new JTextField();
+		txtDiretor.setBounds(119, 79, 86, 20);
+		contentPane.add(txtDiretor);
+		txtDiretor.setColumns(10);
+		
+		txtGenero = new JTextField();
+		txtGenero.setBounds(119, 133, 86, 20);
+		contentPane.add(txtGenero);
+		txtGenero.setColumns(10);
+		
+		JLabel lblGenero = new JLabel("Gênero:");
+		lblGenero.setBounds(63, 136, 46, 14);
+		contentPane.add(lblGenero);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.setBounds(100, 190, 89, 23);
+		contentPane.add(btnNewButton);
 	}
 }
